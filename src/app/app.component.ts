4f44d23b088fbc50ad94244b0944cf5047aea90d
sathiskb <sathish.bala@byvalindia.com>
2021-09-20 10:09:01 +0530

    sathishmasterdesign_v6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,OnChanges,SimpleChanges,Inject,Input} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent   {
+  opened: boolean;
+  public sidebarToggled = false;
   title = 'sequr-user';
+
+  toggleSidebar() {
+    let assidebar = document.querySelector('.sidenav');
+    let body = document.querySelector('body');
+    
+    console.log(assidebar);
+   
+      this.sidebarToggled = !this.sidebarToggled;
+      console.log(this.sidebarToggled );
+      // debugger
+      if(window.innerWidth  < 600){
+        if(assidebar.classList.contains('sidebar' || '' ))    
+        {
+          assidebar.classList.add('sidebar-hidden');
+            body.classList.remove('activemenu');
+            assidebar.classList.remove('sidebar');
+        }
+        else
+        { 
+          assidebar.classList.remove('sidebar-hidden');
+          body.classList.add('activemenu');
+          assidebar.classList.add('sidebar');
+        
+        }
+      }
+  else{
+  if(this.sidebarToggled) {
+    assidebar.classList.add('sidebar-hidden');
+    body.classList.add('activemenu');
+    assidebar.classList.remove('sidebar');
+  } 
+  else {
+    assidebar.classList.remove('sidebar-hidden');
+    body.classList.remove('activemenu');
+    assidebar.classList.add('sidebar');
+  }
+  }  
+  }
 }
