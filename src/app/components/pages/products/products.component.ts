e071ea8c44e9bc7748f5fc065150f633ef28d3d7
sathiskb <sathish.bala@byvalindia.com>
2021-09-22 17:51:19 +0530

    sathishmasterdesign_v14

--- src/app/components/pages/products/products.component.ts
+++ src/app/components/pages/products/products.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  listview(){
+    
+    let asgrid = document.querySelector('.as-mt-main >.as-grid');
+    let asgridg = document.querySelector('.as-g-active');
+    let asgridl = document.querySelector('.as-l-active');
+    asgrid.classList.add('as-list')
+    asgridg.classList.remove('view-active')
+    asgridl.classList.add('view-active')
+    
+  }
+  gridview(){
+    
+    let asgrid = document.querySelector('.as-mt-main >.as-grid');
+    let asgridg = document.querySelector('.as-g-active');
+    let asgridl = document.querySelector('.as-l-active');
+   
+    asgrid.classList.remove('as-list')
+    asgridg.classList.add('view-active')
+    asgridl.classList.remove('view-active')
+  }
 }
