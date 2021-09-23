e11ba63bbc9616ec21a1d568b72ba4bec5caed55
geetha mathiyalagan <geethamathi57@gmail.com>
2021-09-23 17:56:51 +0530

    product

--- src/app/services/shared/enum/enum.util.ts
+++ src/app/services/shared/enum/enum.util.ts
@@ -1,7 +1,10 @@
 enum appModels {
     LOGIN='employee/login',
+    DETAILS='item/getItemById',
+    ADDTOCART='card/add'
 }
-
+//614993f607f4ce431244b852
+//6149b8ec74e1d5ef4317a9b1
 
 
 export { appModels}
