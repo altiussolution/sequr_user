d662c4c61a8be2c27cee5c77c0e132f8cba0c5f3
Priyadharshini <priyadharshini.kathirvel@adcltech.com>
2021-09-25 14:26:18 +0530

    cart_list

--- src/app/components/pages/mycart/mycart.component.ts
+++ src/app/components/pages/mycart/mycart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { ToastrService } from 'ngx-toastr';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-mycart',
@@ -7,26 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MycartComponent implements OnInit {
 
-  constructor() { }
+  cartDetails: any[] = [];
+
+  constructor(private crudService: CrudService, private toast: ToastrService) { }
 
   ngOnInit(): void {
+    this.getCartItems();
   }
-  listview(){
-    
+  getCartItems() {
+    this.crudService.get('cart/myCart').pipe(untilDestroyed(this)).subscribe(res => {
+      console.log(res)
+      this.cartDetails = res[0].cart;
+    }, error => {
+      this.toast.error(error.message);
+    })
+  }
+
+  ngOnDestroy() { }
+
+  listview() {
+
     let asgrid = document.querySelector('.as-mt-main >.as-grid');
     let asgridg = document.querySelector('.as-g-active');
     let asgridl = document.querySelector('.as-l-active');
     asgrid.classList.add('as-list')
     asgridg.classList.remove('view-active')
     asgridl.classList.add('view-active')
-    
+
   }
-  gridview(){
-    
+  gridview() {
+
     let asgrid = document.querySelector('.as-mt-main >.as-grid');
     let asgridg = document.querySelector('.as-g-active');
     let asgridl = document.querySelector('.as-l-active');
-   
+
     asgrid.classList.remove('as-list')
     asgridg.classList.add('view-active')
     asgridl.classList.remove('view-active')
