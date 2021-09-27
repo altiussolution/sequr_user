f4b362d37b23c837f38ddbe1cfb0ed9bd253a085
geetha mathiyalagan <geethamathi57@gmail.com>
2021-09-27 10:08:15 +0530

    pl

--- src/app/components/pages/productslist/productslist.component.ts
+++ src/app/components/pages/productslist/productslist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { CrudService } from 'src/app/services/crud.service';
 import { appModels } from 'src/app/services/shared/enum/enum.util';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-productslist',
@@ -9,13 +10,54 @@ import { appModels } from 'src/app/services/shared/enum/enum.util';
   styleUrls: ['./productslist.component.scss']
 })
 export class ProductslistComponent implements OnInit {
-
-  constructor(public crud:CrudService) { }
+  kit: any=[];
+  page = 0;
+  size = 4;
+  data: any;
+  kitdata: any=[];
+  item: any=[];
+  bin: any=[];
+  compartment: any=[];
+  cube: any=[];
+  constructor(public crud:CrudService,private toast: ToastrService) { }
 
   ngOnInit(): void {
-    
     this.crud.get(appModels.KITTINGLIST).pipe(untilDestroyed(this)).subscribe((res:any) => {
-      console.log(res)})
+      console.log(res)
+      this.kit =res.data
+       for(let i=0;i<this.kit.length;i++){
+        this.kitdata=this.kit[i]?.kit_data
+
+     }
+     for(let i=0;i<this.kitdata.length;i++){
+      this.item=this.kitdata[i]?.item
+      this.bin=this.kitdata[i]?.bin
+      this.compartment=this.kitdata[i]?.compartment
+      this.cube=this.kitdata[i]?.bin
+     }
+    })
+  }
+  addkitcart(_id:any) {
+    this.crud.post(appModels.ADDKITCART + _id).pipe(untilDestroyed(this)).subscribe((res:any) => {
+      console.log(res)
+      if (res != "") {
+        if(res['message']=="Successfully added into cart!"){
+          this.toast.success("cart added successfully")
+        }
+       }
+    },error=>{
+      this.toast.error("cart added Unsuccessfully")
+    })
+  }
+  getData(obj) {
+    let index=0,
+        startingIndex=obj.pageIndex * obj.pageSize,
+        endingIndex=startingIndex + obj.pageSize;
+
+    this.data = this.kit.filter(() => {
+      index++;
+      return (index > startingIndex && index <= endingIndex) ? true : false;
+    });
   }
   listview(){
     
