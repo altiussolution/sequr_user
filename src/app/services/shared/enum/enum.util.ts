7aa01acb10e4fb91f5822d1708606b92d539596f
geetha mathiyalagan <geethamathi57@gmail.com>
2021-10-11 10:03:26 +0530

    src

--- src/app/services/shared/enum/enum.util.ts
+++ src/app/services/shared/enum/enum.util.ts
@@ -14,7 +14,8 @@ enum appModels {
     CHANGEPWD='employee/changePassword/id',
     RETURNCART='cart/return',
     RETURNKIT='kits/return',
-    TAKENOW='machine'
+    TAKENOW='machine',
+    ITEM='item/get'
 }
 //614993f607f4ce431244b852
 //6149b8ec74e1d5ef4317a9b1
