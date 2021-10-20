5ffe0d9650a0660f5783b73bd6afb3cf0500b580
meenakshiva <meenakshi.shivamurthi@adcltech.com>
2021-10-20 11:24:21 +0530

    takeNow item and kit

--- src/app/components/pages/itemhistory/itemhistory.component.ts
+++ src/app/components/pages/itemhistory/itemhistory.component.ts
@@ -11,119 +11,120 @@ import { appModels } from 'src/app/services/shared/enum/enum.util';
   styleUrls: ['./itemhistory.component.scss']
 })
 export class ItemhistoryComponent implements OnInit {
-  itemhistorycart: any=[];
-  itemhistorykit: any=[];
+  itemhistorycart: any = [];
+  itemhistorykit: any = [];
   date: any;
   date1: any;
-  arrayvalue:any=[]
-  DataForm:FormGroup;
+  arrayvalue: any = []
+  DataForm: FormGroup;
   @ViewChild('closebutton') closebutton;
-  arrayvalue1: any=[];
+  arrayvalue1: any = [];
   id: any;
-  cartdata: any=[];
-  constructor(public crud:CrudService, private toast: ToastrService) { }
+  cartdata: any = [];
+  constructor(public crud: CrudService, private toast: ToastrService) { }
   ngOnInit(): void {
-  this.arrayvalue1=[];
-  this.arrayvalue=[];
-  this.crud.get(appModels.ITEMLIST).pipe(untilDestroyed(this)).subscribe((res:any) => {
+    this.arrayvalue1 = [];
+    this.arrayvalue = [];
+    this.crud.get(appModels.ITEMLIST).pipe(untilDestroyed(this)).subscribe((res: any) => {
       console.log(res)
-      this.itemhistorycart=[]
-      this.cartdata=res['Cart'][0]['cart']
-      for(let i=0;i< this.cartdata?.length;i++){
-        if(this.cartdata[i]['cart_status']==2){
-        this.itemhistorycart.push(this.cartdata[i])
-        }}
-      this.id=res['Cart'][0]['_id']
-      this.date=res['Cart'][0]['updated_at']
-      this.itemhistorykit=res['Kits']
-  })
-}
-  passParams(event:any,val:any){
-    if(event.target.checked){
-      this.arrayvalue.push(val);
+      this.itemhistorycart = []
+      this.cartdata = res['Cart'][0]['cart']
+      for (let i = 0; i < this.cartdata?.length; i++) {
+        if (this.cartdata[i]['cart_status'] == 2) {
+          this.itemhistorycart.push(this.cartdata[i])
+        }
       }
+      this.id = res['Cart'][0]['_id']
+      this.date = res['Cart'][0]['updated_at']
+      this.itemhistorykit = res['Kits']
+    })
+  }
+  passParams(event: any, val: any) {
+    if (event.target.checked) {
+      this.arrayvalue.push(val);
+    }
     if (!event.target.checked) {
       let index = this.arrayvalue.indexOf(val);
-    if (index > -1) {
-      this.arrayvalue.splice(index, 1);
+      if (index > -1) {
+        this.arrayvalue.splice(index, 1);
       }
     }
     console.log(this.arrayvalue)
   }
-  delete(i:any){
+  delete(i: any) {
     if (i > -1) {
       this.arrayvalue.splice(i, 1);
-      }
+    }
   }
-  myFunction(event,i){
+  myFunction(event, i) {
     console.log(event.target.id)
-    if(this.arrayvalue[i].qty>=event.target.value){
+    if (this.arrayvalue[i].qty >= event.target.value) {
 
-    }else{
-    (<HTMLInputElement>document.getElementById(event.target.id)).value="";
+    } else {
+      (<HTMLInputElement>document.getElementById(event.target.id)).value = "";
     }
   }
 
-  close(){
+  close() {
     this.ngOnInit();
   }
-  returnproduct(){
-    if(this.arrayvalue.length!=0){
-    var result = this.arrayvalue.map(function(a) {return a?._id;});
-    console.log(this.itemhistorycart["_id"])
-    let data={
-      "cart_id": this.id,
-      "return_items" : result,
-      "cart_status" : 3 ,
-     
-    }
-  this.crud.update2(appModels.RETURNCART,data).pipe(untilDestroyed(this)).subscribe((res:any) => {
-     this.closebutton.nativeElement.click();
-     this.toast.success("Item Returned Successfully")
-     this.ngOnInit();
-    })
-  }else{
-    this.toast.error("Please choose any item from list")
-    this.closebutton.nativeElement.click();
-    this.ngOnInit();
-  }
-  }
-  passParams1(event:any,val:any){
-    if(event.target.checked){
-      this.arrayvalue1.push(val);
+  returnproduct() {
+    if (this.arrayvalue.length != 0) {
+      var result = this.arrayvalue.map(function (a) { return a?._id; });
+      console.log(this.itemhistorycart["_id"])
+      let data = {
+        "cart_id": this.id,
+        "return_items": result,
+        "cart_status": 3,
+
       }
+      this.crud.update2(appModels.RETURNCART, data).pipe(untilDestroyed(this)).subscribe((res: any) => {
+        this.closebutton.nativeElement.click();
+        this.toast.success("Item Returned Successfully")
+        this.ngOnInit();
+      })
+    } else {
+      this.toast.error("Please choose any item from list")
+      this.closebutton.nativeElement.click();
+      this.ngOnInit();
+    }
+  }
+  passParams1(event: any, val: any) {
+    if (event.target.checked) {
+      this.arrayvalue1.push(val);
+    }
     if (!event.target.checked) {
       let index = this.arrayvalue1.indexOf(val);
-    if (index > -1) {
-      this.arrayvalue1.splice(index, 1);
+      if (index > -1) {
+        this.arrayvalue1.splice(index, 1);
       }
     }
     console.log(this.arrayvalue1)
   }
-  returnkits(){
-    if(this.arrayvalue1.length!=0){
-      var kitnames = this.arrayvalue1.map(function(a) {return a?.kit_name;});
+  returnkits() {
+    if (this.arrayvalue1.length != 0) {
+      var kitnames = this.arrayvalue1.map(function (a) { return a?.kit_name; });
       if (confirm(`Are you sure want to Return the Kits for ${kitnames}?`)) {
-          var kitids = this.arrayvalue1.map(function(a) {return a?.update_kit_id;});
-          var cartid = this.arrayvalue1.map(function(a) {return a?.cart_id;});
-          console.log(kitids)
-          let data={
-            "cart_id":cartid,
-          "return_items" : kitids,
-          "kit_status" : 3,
-          }
-    this.crud.update2(appModels.RETURNCART,data).pipe(untilDestroyed(this)).subscribe((res:any) => {
-       this.closebutton.nativeElement.click();
-       this.toast.success("Kit Returned Successfully")
-       this.ngOnInit();
-      })
-    }else{
-      this.ngOnInit();
-    }
-    }else{
+        var kitids = this.arrayvalue1.map(function (a) { return a?.update_kit_id; });
+        var cartid = this.arrayvalue1.map(function (a) { return a?.cart_id; });
+        console.log(kitids)
+        let data = {
+          "cart_id": cartid,
+          "return_items": kitids,
+          "kit_status": 3,
+        }
+        this.crud.update2(appModels.RETURNCART, data).pipe(untilDestroyed(this)).subscribe((res: any) => {
+          this.closebutton.nativeElement.click();
+          this.toast.success("Kit Returned Successfully")
+          this.ngOnInit();
+        })
+      } else {
+        this.ngOnInit();
+      }
+    } else {
       this.toast.error("Please choose any kit from list")
     }
-    
+
   }
 
   //************   Arunkumar  ***********************/
@@ -145,21 +146,45 @@ export class ItemhistoryComponent implements OnInit {
   }
 
   // Format and Clean Objects
-  async formatMachineData() {
+  async formatMachineData(item) {
     let machineData: any[] = []
     console.log('*******  Format Data **********')
-    this.arrayvalue.forEach(async item => {
-      let eachItemForMachines = {}
-      eachItemForMachines['cart_id'] = item._id
-      eachItemForMachines['item_id'] = item.item._id
-      eachItemForMachines['qty'] = item.qty
-      eachItemForMachines['stock_allocation_id'] = item.item_details._id
-      eachItemForMachines['cube_id'] = item.item_details.cube.cube_id
-      eachItemForMachines['column_id'] = item.item_details.bin.bin_id
-      eachItemForMachines['bin_id'] = item.item_details.compartment.compartment_id
-      eachItemForMachines['compartment_id'] = item.item_details.compartment_number
-      await machineData.push(eachItemForMachines)
-    });
+    if (item == 'cart') {
+      this.arrayvalue.forEach(async item => {
+        let eachItemForMachines = {}
+        eachItemForMachines['cart_id'] = item._id || item.cart_id
+        eachItemForMachines['item_id'] = item.item._id
+        eachItemForMachines['qty'] = item.qty
+        eachItemForMachines['stock_allocation_id'] = item.item_details._id
+        eachItemForMachines['cube_id'] = item.item_details.cube.cube_id
+        eachItemForMachines['column_id'] = item.item_details.bin.bin_id
+        eachItemForMachines['bin_id'] = item.item_details.compartment.compartment_id
+        eachItemForMachines['compartment_id'] = item.item_details.compartment_number
+        await machineData.push(eachItemForMachines)
+      });
+    }
+    else if (item == 'kit') {
+      let i = 0
+      this.arrayvalue1[0].kit_item_details.forEach(async item => {
+        let eachItemForMachines = {}
+        eachItemForMachines['cart_id'] = this.arrayvalue1[0].kit_cart_id
+        eachItemForMachines['kit_id'] = this.arrayvalue1[0].kit_id._id
+        // eachItemForMachines['kit_cart_id'] = this.arrayvalue1[0].kit_cart_id
+        eachItemForMachines['item_id'] = item.item._id
+        eachItemForMachines['kit_qty'] = item.qty
+        // eachItemForMachines['qty'] = item.kit_id.kit_data[i].qty
+        eachItemForMachines['stock_allocation_id'] = item._id
+        eachItemForMachines['cube_id'] = item.cube.cube_id
+        eachItemForMachines['column_id'] = item.bin.bin_id
+        eachItemForMachines['bin_id'] = item.compartment.compartment_id
+        eachItemForMachines['compartment_id'] = item.compartment_number
+        await machineData.push(eachItemForMachines)
+        i++
+
+      })
+
+    }
+
     return machineData
 
   }
@@ -214,8 +239,9 @@ export class ItemhistoryComponent implements OnInit {
   // Take Items form machine  
   TakeOrReturnItems: any[] = []
   machinesList = []
-  async returnItem() {
-    let totalMachinesList = await this.formatMachineData()
+  async returnItem(item: string) {
+    let totalMachinesList = await this.formatMachineData(item)
+    console.log(totalMachinesList)
     let machinesList = await this.groupbyData(totalMachinesList)
     console.log(machinesList)
     //call allDevInfo once
@@ -250,7 +276,7 @@ export class ItemhistoryComponent implements OnInit {
           console.log('inside while loop status bin ' + machine.bin_id + status)
           if (status == 'Closed' || status == 'Locked') {
             await this.sleep(9000)
-            await this.crud.post('machine/lockBin', machine).pipe(untilDestroyed(this)).toPromise()            
+            await this.crud.post('machine/lockBin', machine).pipe(untilDestroyed(this)).toPromise()
             machineColumnStatus = true
             await this.TakeOrReturnItems.push(machine)
           }
@@ -287,16 +313,21 @@ export class ItemhistoryComponent implements OnInit {
       await this.updateAfterTakeOrReturn(successTake)
     } else if (successTake.length < totalMachinesList.length) {
       console.log(successTake.length + ' items Taken successfully \n' + successTake.length + ' items failed return')
-      await this.updateAfterTakeOrReturn(successTake)
+      await this.updateAfterTakeOrReturn(successTake, item)
     }
   }
 
   //Update Cart and Stock Allocation documents after item Take/Return
-  async updateAfterTakeOrReturn(successTake: any) {
+  async updateAfterTakeOrReturn(successTake: any, item?) {
     let data = {
       cart_id: this.id,
       take_items: successTake,
-      cart_status: 3
+    }
+    if (item == 'cart') {
+      data['cart_status'] = 3
+    } else {
+      data['kit_status'] = 3
+
     }
     this.crud.post(`cart/updateReturnTake`, data).pipe().subscribe(async (res) => {
       console.log(res)
@@ -337,5 +368,5 @@ export class ItemhistoryComponent implements OnInit {
   // machineColumnStatus
 
 
-  ngOnDestroy(){}
+  ngOnDestroy() { }
 }
